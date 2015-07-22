c316a4a88f1525d30935e2f09536026357eb8063
smalab <oitsmalab@gmail.com>
2015-07-22 15:04:14 +0900

    テキストとボタンの配置

---
+++ avater/Assets/Next_Scene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Next_Scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+public static void Next (){ 
+		//Application.loadedLevel("次のシーンに移りたい奴");
+		Debug.Log ("touch");
+	}
+}
