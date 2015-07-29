0b675e7eb59134ebaf24c60290cd98c904cf8599
yanoty3d <vzb10326@me.com>
2015-07-29 10:28:03 +0900

    RealSenseの音声認識の導入を試みる。クラッシュが多い。シーンごとに新たに音声認識を呼び出すせいか。音声認識の部分はデストロイせずにシーン遷移するべきか。

---
+++ avater/Assets/Scripts/VoiceButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class VoiceButton : MonoBehaviour {
+
+    public UnityEngine.EventSystems.EventTrigger.TriggerEvent voiceButton;
+
+    public string textToHear;
+
+    private SpeechRecognition speechRecognition;
+    BaseEventData eventData;
+
+    // Use this for initialization
+    void Start () {
+        speechRecognition = GameObject.Find("Speech").GetComponent<SpeechRecognition>();
+        eventData = new BaseEventData(EventSystem.current);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (speechRecognition.currentText == textToHear)
+        {
+            speechRecognition.currentText = "";
+            voiceButton.Invoke(eventData);
+        }
+	}
+}
