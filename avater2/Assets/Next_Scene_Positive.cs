a8b86ef0295e235fd17ebea63bde889c04fd3553
oitsmalab <oitsmalab@gmail.com>
2015-07-31 16:44:03 +0900

    ＃１７問題数10問、心の表情のスプライトの変更できた。

--- avater2/Assets/Next_Scene_Positive.cs
+++ avater2/Assets/Next_Scene_Positive.cs
@@ -33,7 +33,7 @@ public class Next_Scene_Positive : MonoBehaviour {
 
 		scene = Application.loadedLevel;
 		while (scene == Application.loadedLevel) {
-			scene = Random.Range (1, 10);
+			scene = Random.Range (1, 11);
 		}
 		Application.LoadLevel(scene);
 		Debug.Log ("touch");
