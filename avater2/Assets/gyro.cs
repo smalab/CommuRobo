1a2f2fd25132ca48115ebd90494ac61ab373fb12
oitsmalab <oitsmalab@gmail.com>
2015-08-04 15:31:57 +0900

    ジャイロの値は取れた

---
+++ avater2/Assets/gyro.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gyro : MonoBehaviour {
+
+
+	void Start () {
+		Input.gyro.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (Input.gyro.rotationRateUnbiased.x);
+
+
+		//Debug.Log ("attiude " + q);
+
+	}
+}
---
+++ avater2/Assets/gyrocontroller.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gyrocontroller : MonoBehaviour {
+	private string gyro;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+public static void Ongyro(){
+		Input.gyro.enabled = true;
+	}
+
+public static void Offgyro(){
+		Input.gyro.enabled = false;
+	}
+
+	public static float y_gyro(){
+		return Input.gyro.rotationRateUnbiased.y;
+	}
+
+		// Update is called once per frame
+	void Update () {
+
+	}
+}
