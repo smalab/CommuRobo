692ac8367ed209d656f5ff258cde83ff994f96b7
oitsmalab <oitsmalab@gmail.com>
2015-08-19 14:55:26 +0900

    #18 加速度センサーを使って、情報は取ることができたが、ipodを横画面で固定できていないので縦画面でとりあえずやる

---
+++ avater2/Assets/gyro_gravity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gyro_gravity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 acceleration = Input.acceleration;
+		Debug.Log (acceleration);
+	}
+}
