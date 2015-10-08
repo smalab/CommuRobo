c8585b15c4446430ad592bf0f94e9055bb247c94
oitsmalab <oitsmalab@gmail.com>
2015-10-08 14:40:35 +0900

    #23スクリプトを作成してアタッチしたがアニメーションが動いているバーが出てこなかった。また、トリガーが全てたってしまうのが難点

--- avater2/Assets/Get_Acceleration.cs
+++ avater2/Assets/Get_Acceleration.cs
@@ -2,15 +2,54 @@
 using System.Collections;
 
 public class Get_Acceleration : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-	
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 acceleration = Input.acceleration;
 		Debug.Log (acceleration);
+
+		if (acceleration.x <= 0.2 && acceleration.y <= -0.9) {
+			SetStand();
+		}
+		if (acceleration.x >= -0.2 && acceleration.x <= 0.2 && acceleration.y >= 0.8 && acceleration.y <= 1.1 && acceleration.z <= 0.2) {
+			SetReverse();
+		}
+		if (acceleration.x >= 0.4 && acceleration.x <= 0.9 && acceleration.y >= -0.7 && acceleration.y <= 0.2) {
+			SetRight();
+		}
+		if (acceleration.x <= -0.4 && acceleration.x >= -0.9 && acceleration.y >= -0.7 && acceleration.y <= 0.2) {
+			SetLeft();
+		}
+		if (acceleration.z <= -1.0) {
+			SetSupine();
+		}
+		if (acceleration.z >= 1.0) {
+			SetProne();
+		}
+
+	}
+
+	public void SetStand(){
+		this.GetComponent<Animator>().SetTrigger("stand");
+	}
+	public void SetReverse(){
+		this.GetComponent<Animator>().SetTrigger("reverse");
+	}
+	public void SetRight(){
+		this.GetComponent<Animator>().SetTrigger("right");
+	}
+	public void SetLeft(){
+		this.GetComponent<Animator>().SetTrigger("left");
+	}
+	public void SetSupine(){
+		this.GetComponent<Animator>().SetTrigger("supine");
+	}
+	public void SetProne(){
+		this.GetComponent<Animator>().SetTrigger("prone");
 	}
 }
