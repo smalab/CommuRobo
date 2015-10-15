5345dfa42898dc7430cf3945ca5ff987c01786d0
oitsmalab <oitsmalab@gmail.com>
2015-10-15 16:25:21 +0900

    #23 アニメータのパラメータでフラグを作ると立つことはたった。しかし、意味がわからないところでたつ。謎です。

--- avater2/Assets/BackRoll.cs
+++ avater2/Assets/BackRoll.cs
@@ -5,10 +5,7 @@ public class BackRoll: StateMachineBehaviour {
 	public AnimatorStateInfo stateInfo1;
  	public AnimatorStateInfo stateInfo2;
 
-	public bool flag1 = false;
-	public bool flag2 = false;
-	public bool flag3 = false;
-	public bool flag4 = false;
+
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -16,43 +13,35 @@ public class BackRoll: StateMachineBehaviour {
 
 
 		if(stateInfo1.IsName("Base Layer.Stand")  && stateInfo2.IsName("Base Layer.Supine")){
-			flag1 = true;
+
+			Get_Acceleration.anim.SetTrigger("flagA");
 		}
 		if(stateInfo1.IsName("Base Layer.Supine") && stateInfo2.IsName("Base Layer.Reverse")){
-			flag2 = true;
+
 		}
 		if(stateInfo1.IsName("Base Layer.Reverse")  && stateInfo2.IsName("Base Layer.Prone")){
-			flag3 = true;
+	
 		}
 		if(stateInfo1.IsName("Base Layer.Prone")  && stateInfo2.IsName("Base Layer.Stand")){
-			flag4 = true;
+	
 		}
 
+
+
+		stateInfo1 = stateInfo2;
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		if(stateInfo1.IsName("Base Layer.Stand")  && stateInfo2.IsName("Base Layer.Supine")){
-			flag1 = true;
-		}
-		if(stateInfo1.IsName("Base Layer.Supine") && stateInfo2.IsName("Base Layer.Reverse")){
-			flag2 = true;
-		}
-		if(stateInfo1.IsName("Base Layer.Reverse")  && stateInfo2.IsName("Base Layer.Prone")){
-			flag3 = true;
-		}
-		if(stateInfo1.IsName("Base Layer.Prone")  && stateInfo2.IsName("Base Layer.Stand")){
-			flag4 = true;
-		}
-	}
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		stateInfo1 = Get_Acceleration.anim.GetCurrentAnimatorStateInfo (0);
-	}
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//	stateInfo1 = stateInfo2;
+	//}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+/*	override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		if(stateInfo1.IsName("Base Layer.Stand")  && stateInfo2.IsName("Base Layer.Supine")){
 			flag1 = true;
 		}
@@ -65,7 +54,7 @@ public class BackRoll: StateMachineBehaviour {
 		if(stateInfo1.IsName("Base Layer.Prone")  && stateInfo2.IsName("Base Layer.Stand")){
 			flag4 = true;
 		}
-	}
+	}*/
 
 	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
 	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
