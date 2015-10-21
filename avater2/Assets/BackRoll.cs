d6318fa585e820ed7e8146d5452e5341df3027c0
oitsmalab <oitsmalab@gmail.com>
2015-10-21 12:32:09 +0900

    #23 ステートに遷移してきたときに番号を割り振る（立ちなら1みたいに・・・）そして、その番号をキューかリストに突っ込むようにしたいと考え中

--- avater2/Assets/BackRoll.cs
+++ avater2/Assets/BackRoll.cs
@@ -9,8 +9,6 @@ public class BackRoll: StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		stateInfo1 = Get_Acceleration.anim.GetCurrentAnimatorStateInfo (0);
-
 
 		//stateInfo2 = Get_Acceleration.anim.GetCurrentAnimatorStateInfo (0);
 
--- avater2/Assets/Get_Acceleration.cs
+++ avater2/Assets/Get_Acceleration.cs
@@ -5,6 +5,14 @@ public class Get_Acceleration : MonoBehaviour {
 	// Use this for initialization
 
 	public static Animator anim;
+
+	public int standflag = 0;
+	public int reverseflag = 0;
+	public int rightflag = 0;
+	public int leftflag = 0;
+	public int supineflag = 0;
+	public int proneflag = 0;
+
 	void Start () {
 		anim = GetComponent<Animator> ();
 
@@ -15,23 +23,29 @@ public class Get_Acceleration : MonoBehaviour {
 		Vector3 acceleration = Input.acceleration;
 		Debug.Log (acceleration);
 
-		if (acceleration.x <= 0.2 && acceleration.y <= -0.9) {
+		if (acceleration.x <= 0.1 && acceleration.y <= -0.9) {
 			SetStand();
+			standflag = 1;
 		}
-		if (acceleration.x >= -0.2 && acceleration.x <= 0.2 && acceleration.y >= 0.8 && acceleration.y <= 1.1 && acceleration.z <= 0.2) {
+		if (acceleration.x >= -0.1 && acceleration.x <= 0.1 && acceleration.y >= 0.8 && acceleration.y <= 1.1 && acceleration.z <= 0.2) {
 			SetReverse();
+			reverseflag = 2;
 		}
 		if (acceleration.x >= 0.8 && acceleration.x <= 1.0 && acceleration.y >= -0.7 && acceleration.y <= 0.2) {
 			SetRight();
+			rightflag = 3;
 		}
 		if (acceleration.x <= -0.8 && acceleration.x >= -1.0 && acceleration.y >= -0.7 && acceleration.y <= 0.2) {
 			SetLeft();
+			leftflag = 4;
 		}
 		if (acceleration.z <= -0.95) {
 			SetSupine();
+			supineflag = 5;
 		}
 		if (acceleration.z >= 0.95) {
 			SetProne();
+			proneflag = 6;
 		}
 	
 	}
@@ -61,6 +75,15 @@ public class Get_Acceleration : MonoBehaviour {
 		anim.ResetTrigger ("left");
 		anim.ResetTrigger ("supine");
 		anim.ResetTrigger ("prone");
+	}
+
+	public static void ResetFrag(){
+		standflag = 0;
+		reverseflag = 0;
+		rightflag = 0;
+		leftflag = 0;
+		supineflag = 0;
+		proneflag = 0;
 	}
 
 	public static void ResetStand(){
