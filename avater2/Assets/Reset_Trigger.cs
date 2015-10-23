561c74f0b609524dadb6ba34fc98611ebd779c7c
oitsmalab <oitsmalab@gmail.com>
2015-10-23 14:41:36 +0900

    #23 全部取れた

--- avater2/Assets/Reset_Trigger.cs
+++ avater2/Assets/Reset_Trigger.cs
@@ -6,7 +6,7 @@ public class Reset_Trigger : StateMachineBehaviour {
 	//  OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		Get_Acceleration.ResetTrigger();
-		Get_Acceleration.ResetFrag ();
+
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -15,9 +15,9 @@ public class Reset_Trigger : StateMachineBehaviour {
 	//}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Get_Acceleration.ResetFrag ();
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
